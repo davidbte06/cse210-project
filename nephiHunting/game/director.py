6b04bf3418dbba566026384f45617f69f9efebe7
davidbte06 <davidbte06@gmail.com>
2021-10-21 00:00:31 -0500

    added logo

--- nephiHunting/game/director.py
+++ nephiHunting/game/director.py
@@ -88,7 +88,7 @@ class Director(arcade.Window):
 
         #Menu
         self.background_gameover = arcade.load_texture("nephiHunting/assets/images/bg2.png")
-
+        # self.logo = arcade.load_texture("nephiHunting/assets/images/logo.jpg")
         # self.scene.add_sprite("Arrow", self.arrow_sprite)
 
     def draw_menu(self):
@@ -100,8 +100,16 @@ class Director(arcade.Window):
             self.background_gameover
         )
 
+        #Draw Logo
+        self.logo_list = arcade.SpriteList()
+        self.logo = arcade.Sprite("nephiHunting/assets/images/logo.jpg", 0.5)
+        self.logo.center_x = self.SCREEN_WIDTH * 0.5
+        self.logo.center_y = self.SCREEN_HEIGHT * 0.6
+        self.logo_list.append(self.logo)
+        self.logo_list.draw()
+
         output = "Press <ENTER> To Start"
-        arcade.draw_text(output, 300, 325, arcade.color.WHITE, 24,) 
+        arcade.draw_text(output, 300, 125, arcade.color.WHITE, 24,) 
 
     def on_draw(self):
         """Render the screen"""
