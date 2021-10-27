ca4f70944317c3cbd3d89fb622a60504172e2300
Lia Ayos <roxana_ayos@hotmail.com>
2021-10-26 23:20:35 -0500

    set path for constants

--- nephiHunting/__main__.py
+++ nephiHunting/__main__.py
@@ -4,9 +4,11 @@ Entrypoint of the application. Calls Director and arcade run
 
 import arcade
 from game.director import Director
+import os
 
 def main():
     """Main function"""
+    os.chdir(os.path.dirname(os.path.realpath(__file__)))
     window = Director()
     window.setup()
     arcade.run()
